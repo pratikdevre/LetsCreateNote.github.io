93ba95188a87b90645a76828afaa0b335012eb79
pratikdeore <58384739+pratikdeore@users.noreply.github.com>
2020-11-21 15:27:36 +0530

    updated on 21th Nov

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-shownotes();
+showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
+
     let addNote = document.getElementById("addNote");
     let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
@@ -14,31 +15,36 @@ addBtn.addEventListener("click", function (e) {
         title: addTitle.value,
         text: addNote.value
     }
-    notesObj.push(myObj);
-    localStorage.setItem("notes", JSON.stringify(notesObj));
-    addNote.value = "";
-    addTitle.value = "";
-    shownotes();
+    if(addNote.value.trim() != '' && addTitle.value.trim() != ''){
+        notesObj.push(myObj);
+        localStorage.setItem('notes', JSON.stringify(notesObj));
+        addNote.value = "";
+        addTitle.value = "";
+    }
+    else{
+        alert("Write something!!!")
+    }
+    showNotes();
 });
 
-
-function shownotes() {
+function showNotes(){
     let notes = localStorage.getItem("notes");
-    if (notes == null) {
+    if(notes == null){
         notesObj = [];
     }
-    else {
+    else{
         notesObj = JSON.parse(notes);
     }
+
     let html = "";
-    notesObj.forEach(function (element, index) {
+    notesObj.forEach(function(element, index){
         html += `
-        <div class="notecard" style="border:2px black solid; background-color: rgb(228, 169, 169);; max-width: 40em; margin-top:5px; display:inline-block">
-            <h3>${element.title}</h3>  
-                <p style=" border: 2px black double; max-width: 30em; background-color:white;   border-radius: 5px; ">${element.text}</p>
-                <br>
-            <button id="${index}" onclick="deleteNote(this.id)" class="delete">Delete Note</button>
-             </div>`;
+                <div id="notes" class="notecard">
+                    <h3>${element.title}</h3>
+                    <p>${element.text}</p>
+                    <br>
+                    <button id="${index}" onclick="deleteNote(this.id)" class="btn">Delete Note</button>
+                </div>`;
     });
     let notesElm = document.getElementById('notes');
     if (notesObj.length != 0) {
@@ -59,5 +65,5 @@ function deleteNote(index) {
     }
     notesObj.splice(index, 1);
     localStorage.setItem("notes", JSON.stringify(notesObj));
-    shownotes();
+    showNotes();
 }
